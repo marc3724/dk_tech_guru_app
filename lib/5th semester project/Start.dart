30d25b7b4b1f2f0858cb60e890fa3d98a1dcaa02
Marc3724 <marc402m@edu.easj.dk>
2023-12-05 11:16:33 +0100

    updatet filestructure

--- lib/5th semester project/Start.dart
+++ lib/5th semester project/Start.dart
@@ -1,9 +1,8 @@
-import 'package:dk_tech_guru_app/5th%20semester%20project/Tasks.dart';
+import 'package:dk_tech_guru_app/5th%20semester%20project/Business/Entities/Tasks.dart';
 
 void main(){
 
   //Task myTask = Task.test(1,"fuck");
-
   Task my2ndTask = Task.defaultConstructor(1, 999, "f soceity", "gong to hell", DateTime(2224,12,5,12,0), false, true);
   int id = my2ndTask.taskID;
   print(id);
@@ -12,11 +11,15 @@ void main(){
   print(my2ndTask.taskID);
 
   Task task1 = Task.defaultConstructor(1, 10, 'Task 1', 'Description 1', DateTime.now(), false, true);
+  List<Task> myTaskList = [task1,my2ndTask];
 
-    Task fuckThisTask  = Task.test(666, "fuckYeah");
-    print(fuckThisTask.dueDate);
-    fuckThisTask.dueDate=DateTime(2023,12,5);
-    print(fuckThisTask.dueDate);
+  Task fuckThisTask  = Task.test(666, "fuckYeah");
+  print(fuckThisTask.dueDate);
+  fuckThisTask.dueDate=DateTime(2023,12,5);
+  print(fuckThisTask.dueDate);
+
+  myTaskList.add(fuckThisTask);
+  myTaskList.removeAt(1);
     // task2 has custom values for taskPointValue and doesRepeat, the rest will use default values
 
  /* List<Task> myList = [myTask,my2ndTask];
