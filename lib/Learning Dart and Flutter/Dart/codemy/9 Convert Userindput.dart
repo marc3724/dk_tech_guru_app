5c36c1a2d9a2d255fa797fab76096e4ccb124751
Lee <Hansendennism@gmail.com>
2023-12-05 11:37:08 +0100

    f

--- lib/Learning Dart and Flutter/Dart/codemy/9 Convert Userindput.dart
+++ lib/Learning Dart and Flutter/Dart/codemy/9 Convert Userindput.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main(){
   print("enter a numbekr");
 
-  var nummy = stdin.readLineSync();
+  var nummyjh = stdin.readLineSync();
   var nummy2 = int.parse(nummy??'0') + 10;
   print("$nummy + 10 = $nummy2");
 
