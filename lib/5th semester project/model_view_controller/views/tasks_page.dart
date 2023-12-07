6f28dba5c62daa4f0891c94beb4ffa8a0e2107eb
Lee <Hansendennism@gmail.com>
2023-12-07 13:08:40 +0100

    new stuf

---
+++ lib/5th semester project/model_view_controller/views/tasks_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TaskPage extends StatelessWidget {
+  const TaskPage
+({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
