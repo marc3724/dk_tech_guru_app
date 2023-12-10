727804b4972947d90fcb341ebf3d938f286ada4d
Marc3724 <marc402m@edu.easj.dk>
2023-12-10 12:31:42 +0100

    we can now generate visuals in tasks_page.dart
    but need to fix Date

--- lib/5th_semester_project/model_view_controller/mvc_start.dart
+++ lib/5th_semester_project/model_view_controller/mvc_start.dart
@@ -5,32 +5,40 @@ import 'models/group.dart';
 import 'controllers/task_controller.dart';
 
 void main() {
-  TaskController taskController = TaskController();
+  TaskController myTaskController = TaskController();
 
-  Task task1 = Task.defaultConstructor(1, 10, 'Task 1', 'Description 1', DateTime.now(), false, true);
-  taskController.createTask(task1);
+  //Task task1 = Task.defaultConstructor(1, 10, 'Task 1', 'Description 1', DateTime.now(), false, true);
+  myTaskController.createTask(1, 10, 'Task 1', 'Description 1', DateTime.now(), false, true);
 
-  Task task2 = Task.defaultConstructor(2, 15, 'Task 2', 'Description 2', DateTime.now(), false, true);
-  taskController.createTask(task2);
+  //Task task2 = Task.defaultConstructor(2, 15, 'Task 2', 'Description 2', DateTime.now(), false, true);
+  myTaskController.createTask(2, 15, 'Task 2', 'Description 2', DateTime.now(), false, true);
 
-  Task task3 = Task.test(3, "Task 3 fuckers",taskDescription: "hello");
+  //Task task3 = Task.test(3, "Task 3 fuckers",taskDescription: "hello");
+  //myTaskController.createTask(task3);
+
   // Hent og udskriv alle opgaver
-  print(taskController.getTasks());
+  print(myTaskController.getTasks());
 
   // Opdater en opgave
-  task3.taskName = 'Updated Task 3';
-  taskController.updateTask(2,taskDescription: "hello bitches");
+  myTaskController.updateTask(3,taskDescription: "new and updated description");
+  myTaskController.updateTask(2,taskDescription: "hello bitches");
 
   // Hent og udskriv alle opgaver igen
-  print(taskController.getTasks());
+  print(myTaskController.getTasks());
 
   // Slet en opgave
-  taskController.deleteTask(1);
+  //myTaskController.deleteTask(1);
 
   // Hent og udskriv alle opgaver efter sletning
-  print(taskController.getTasks());
+  print(myTaskController.getTasks());
 
+  List<Task> myList = myTaskController.getTasks();
+  print("length of mylist = ${myList.length}");
+  for (int i = 0; i < myList.length; i++) {
+    print("this is task nr ${myList[i].taskID.toString()}" );
 
+    // Your code here
+  }
 
   Group_Members user1 = Group_Members(membersId: 1, membersname: 'User1');
   Group_Members user2 = Group_Members(membersId: 2, membersname: 'User2');
@@ -52,5 +60,7 @@ void main() {
 
   print('GroupA Members: ${group1.members.map((user) => user.membersname)}');
   print('GroupB Members: ${group2.members.map((user) => user.membersname)}');
+
+
 
 }
--- lib/5th_semester_project/model_view_controller/views/tasks_page.dart
+++ lib/5th_semester_project/model_view_controller/views/tasks_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../controllers/task_controller.dart';
+import '../models/tasks.dart';
 
 class TaskPage extends StatefulWidget {
   TaskPage({super.key});
@@ -10,47 +12,86 @@ class TaskPage extends StatefulWidget {
 class _TaskPageState extends State<TaskPage> {
   TextEditingController myController = TextEditingController();
 
+
+  /*@override
+  void initState() {
+    super.initState();
+    taskController = TaskController(); // Initialize the TaskController instance
+    myTaskList = taskController.getTasks(); // Call the method on the instance
+
+  }*/
+
   void getText() {
     print(myController.text);
   }
 
+  void getTasks(List<Task> taskList){
+    List<Task> myTaskList = taskList;
+  }
+  //a list for saving Tasks
+
+  //List<Task> taskList = TaskController.getTasks().cast<Task>();
+
+  /*void generateTaskFromList(List<Task> list){
+    for (int i = 0; i < list.length; i++) {
+
+
+
+    }
+  }*/
+
+
   @override
   Widget build(BuildContext context) {
+    TaskController myTaskController = TaskController();
+    myTaskController.createTask(1, 10, 'Task 1', 'Description 1', DateTime.now(), false, true);
+
+    //Task task2 = Task.defaultConstructor(2, 15, 'Task 2', 'Description 2', DateTime.now(), false, true);
+    myTaskController.createTask(2, 15, 'Task 2', 'Description 2', DateTime.now(), false, true);
+    List myTaskList = myTaskController.getTasks();
+    
     return Scaffold(
       appBar: AppBar(
-        title: Text("welcome"),
+        title: Text("ToDo App"),
         backgroundColor: Colors.lightGreenAccent,
       ),
       body: Column(
         children: [
+          //
           Container(
             color: Colors.lightBlueAccent,
-            child: const Row(
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text("title "),
-                Text("description "),
-                Text("points "),
-                Text("weekday "),
-                Text("date"),
-                //ElevatedButton(onPressed: getText, child: Text("hello")
-                //),
+                Text("Title"),
+                Text("Description"),
+                Text("Points"),
+                //Text("Weekday"),
+                Text("Date"),
               ],
             ),
           ),
-          Container(
-            color: Colors.lightBlueAccent,
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text("mytitle "),
-                Text("mydescription "),
-                Text("mypoints "),
-                Text("myweekday "),
-                Text("mydate"),
-                //ElevatedButton(onPressed: getText, child: Text("hello")
-                //),
-              ],
+          //TODO write about this in the rapport
+          Expanded(
+            child: ListView.builder(
+              itemCount: myTaskList.length,
+              itemBuilder: (context, index) {
+                Task task = myTaskList[index];
+                return Container(
+                  color: Colors.lightBlueAccent,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(task.taskName),
+                      Text(task.taskDescription),
+                      Text(task.taskPointValue.toString()),
+                      Text(task.dueDate.toString()),
+
+                      // Add more Text widgets based on task properties
+                    ],
+                  ),
+                );
+              },
             ),
           ),
         ],
